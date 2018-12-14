a4a1283fdd998d1a0bb5e621613a38ebb99560ad
robertmccarson <45802069+robertmccarson@users.noreply.github.com>
2018-12-14 15:45:50 -0500

    Update google-sheet.js

--- google-sheet.js
+++ google-sheet.js
@@ -24,7 +24,7 @@ $("#foo").submit(function(event){
 
     // Fire off the request to /form.php
     request = $.ajax({
-        url: "SCRIPT URL GOES HERE",
+        url: "https://script.google.com/macros/s/AKfycbymCjqem2S4WZ1VO_sk3hrSZYRDK6vSLD7-TpDJf8w_bUXwtvw/exec",
         type: "post",
         data: serializedData
     });
