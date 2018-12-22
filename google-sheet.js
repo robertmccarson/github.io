10aeb0dfd7736a7119ae48957801c9cd4cccccf4
robertmccarson <45802069+robertmccarson@users.noreply.github.com>
2018-12-21 22:38:35 -0500

    Update google-sheet.js

--- google-sheet.js
+++ google-sheet.js
@@ -29,15 +29,14 @@ $("#foo").submit(function(event){
         data: serializedData
     });
 
-    var url = 'https://script.google.com/macros/s/AKfycbw1CW2KcMOnXFY3Z27fCyxTD_HsJt39SqbUjdWtaedhoyQe_oI/exec';
-        var redirectUrl = 'response.html';
-        // show the loading
-        $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
-        var jqxhr = $.post(url, $form.serialize(), function(data) {
-added this >>> jqxhr.crossDomain = true;
-console.log("Success! Data: " + data.statusText);
-$(location).attr('href',redirectUrl);
-        })
+   // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
 
     // Callback handler that will be called on failure
     request.fail(function (jqXHR, textStatus, errorThrown){
